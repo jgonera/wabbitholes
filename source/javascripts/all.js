cafa807f6091dfb514619146586ab95f91d0dbb3
Juliusz Gonera <jgonera@wikimedia.org>
2014-08-07 17:16:22 -0700

    Swipe, not scroll
    No touch gestures yet, coming soon.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,34 @@
 //= require_tree .
 
-$('#wrapper').fullpage({
-  easing: "linear",
-  verticalCentered: false
+;(function($) {
+  function Swipe(el) {
+    var $el = $(el).addClass('swipe-container');
+
+    this.currentSlide = 0;
+    this.$slides = $el.children().addClass('swipe-slide');
+    this.$slides.eq(this.currentSlide).addClass('swipe-active');
+  }
+
+  Swipe.prototype.next = function() {
+    ++this.currentSlide;
+    this.$slides.removeClass('swipe-active');
+    this.$slides.eq(this.currentSlide).addClass('swipe-active');
+  };
+
+  Swipe.prototype.prev = function() {
+    --this.currentSlide;
+    this.$slides.removeClass('swipe-active');
+    this.$slides.eq(this.currentSlide).addClass('swipe-active');
+  };
+
+  $.Swipe = Swipe;
+}(jQuery || Zepto));
+
+var swipe = new $.Swipe('#slider');
+
+$('.previous').on('click', function() {
+  swipe.prev();
+});
+$('.next').on('click', function() {
+  swipe.next();
 });
